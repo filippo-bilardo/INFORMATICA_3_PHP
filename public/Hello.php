c3e0f2543297e25ca10bb85b022ed717746802fb
Filippo <filippo.bilardo@tiscali.it>
2022-09-22 10:05:43 +0000

    updete

---
+++ public/Hello.php
@@ -0,0 +1,18 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php 
+    echo '<p>Hello World</p>'; 
+    $a = "Ciao";
+    $b = "Mondo";
+    $c = "1";
+    $d = "aaa";
+    echo $a.$b;
+    echo "<br>----";
+    echo $c + $d;
+    echo "<br>----";
+ ?> 
+ </body>
+</html>
