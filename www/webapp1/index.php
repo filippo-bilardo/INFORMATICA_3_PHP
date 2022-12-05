afa7b9c01785875b6f3180fc1df48ad6c9fe9d10
Filippo <filippo.bilardo@tiscali.it>
2022-12-05 22:45:56 +0000

    webapp1

--- www/webapp1/index.php
+++ www/webapp1/index.php
@@ -21,9 +21,9 @@
   <h1>WEBAPP1</h1>
   <h2>Gestire le sessioni nelle applicazioni web</h2>
   <p>
-    <a href="riservata.php" class="btn btn-warning">Vai all'area riservata del sito</a>
+    <a href="riservata.php">Vai all'area riservata del sito</a>
     <br/>
-    <a href="login.php" class="btn btn-danger">Login</a>
+    <a href="login.php">Login</a>
   </p>
 </body>
 </html>
