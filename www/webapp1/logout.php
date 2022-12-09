a058fc94f8dc0d8ec89bf15b341588aaba547fbb
Filippo <filippo.bilardo@tiscali.it>
2022-12-09 08:23:23 +0000

    webapp1

--- www/webapp1/logout.php
+++ www/webapp1/logout.php
@@ -15,9 +15,9 @@ include 'functions.php';
 
 session_start();
 $_SESSION = array(); // Elimina tutti i valori della sessione.
-session_destroy(); // Cancella la sessione.
+//session_destroy(); // Cancella la sessione.
 
-session_start();
-$_SESSION['pag_count']=0;
+//session_start();
+//$_SESSION['pag_count']=0;
 header('Location: index.php');
 ?>
