0856c5196020093142123dd3c003abaa8c8d07f4
Filippo <filippo.bilardo@tiscali.it>
2022-12-28 18:40:22 +0000

    update

--- www/ES04/logout.php
+++ www/ES04/logout.php
@@ -1,11 +1,19 @@
 <?php
 // Avvia una nuova sessione
 session_start();
-
 // Distrugge la sessione attiva
 session_destroy();
-
 // Redirigi l'utente verso la pagina di login
-header("Location: login.php");
-exit;
+//header("Location: login.php");
+//exit;
 ?>
+<html>
+<head>
+	<title>Logout</title>
+</head>
+<body>
+	<h2>Sessione chiusa correttamente</h2>
+	<a href="login.php">Accedi di nuovo.</a>
+	<a href="index.php">Torna alla home.</a>
+</body>
+</html>
