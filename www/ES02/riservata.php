0e7098b0cf8b40299c1eb5d13b9beadc22032ee1
filippo-bilardo <filippo.bilardo@tiscali.it>
2023-09-29 07:33:17 +0000

    accesso a pagina riservata

--- www/ES02/riservata.php
+++ www/ES02/riservata.php
@@ -3,13 +3,22 @@
     <h1>Verifica dati inseriti</h1>
 
     <?php
+    $nome = $_GET['nome'];
+    $pwd = $_GET["password"];
 
-    $Nome = $_GET['nome'];
-    $Password = $_GET["password"];
-    
-    echo "Benvenuto $Nome<br>\n";
-    echo "Hai inserito come password $Password";
+    echo "Dati inseriti tramite il form html:<br/>\n";
+    echo "nome = $nome<br/>\n";
+    echo "password = $pwd<br/>\n";
+    echo "<br/><br/>\n";
 
+    if($nome!="admin" && $pwd!='passwd')
+    {
+        echo "<h4>Attenzione! Nome utente o password sbagliate</h4>";
+    }
+    else
+    {
+        echo "<h4>Benvenuto $nome <br/>Nell'area riservata del sito!</h4>";
+    }
     ?>
 </body>
 </html>
