4f3d892c5b67f536a9deaa811db8a9dbb7ecdd52
filippo-bilardo <filippo.bilardo@tiscali.it>
2024-10-10 10:01:10 +0000

    update

--- www/ES03/es_heredoc.php
+++ www/ES03/es_heredoc.php
@@ -1,5 +1,14 @@
 <!-- http://204.216.213.176/inf3php/ES03/es_heredoc.php -->
 <?php
+$form = <<<FORM
+<form method="post">
+  <label for="nome">Nome:</label><input type="text" id="nome" name="nome"><br>
+  <label for="cognome">Cognome:</label><input type="text" id="cognome" name="cognome"><br>
+  <label for="email">Email:</label><input type="email" id="email" name="email"><br>
+  <input type="submit" value="Invia">
+</form>
+FORM;
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $nome = $_POST['nome'];
   $cognome = $_POST['cognome'];
@@ -11,14 +20,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   TESTO;
   $html = $stringa;
 } else {
-  $form = <<<FORM
-    <form method="post">
-      <label for="nome">Nome:</label><input type="text" id="nome" name="nome"><br>
-      <label for="cognome">Cognome:</label><input type="text" id="cognome" name="cognome"><br>
-      <label for="email">Email:</label><input type="email" id="email" name="email"><br>
-      <input type="submit" value="Invia">
-    </form>
-  FORM;
   $html = $form;
 }
 ?>
--- www/ES03/es_postback1.php
+++ www/ES03/es_postback1.php
@@ -1,18 +1,28 @@
 <!-- http://204.216.213.176/inf3php/ES03/es_postback1.php -->
 <?php
+echo 'var_dump(isset($_POST[\'nome\']));';
+var_dump(isset($_POST['nome']));
 
+echo '<br><br>var_dump(isset($_POST[\'nome\']));';
+var_dump(isset($_POST));
+
+echo '<br><br>var_dump(isset($_POST[\'submit\']));';
+isset($_POST['submit']) ? var_dump($_POST['submit']) : var_dump('submit non settato');
+
+echo '<br><br>';
 //Stampiamo tutte le chiavi dell'array $_POST 
 // e anche il valore associato a ciascuna chiave
 foreach ($_POST as $key => $value) {
   echo "Chiave: $key, Valore: $value <br>";
 }
 
+
 $form = <<<FORM
   <form method="post">
     <label for="nome">Nome:</label><input type="text" id="nome" name="nome"><br>
     <label for="cognome">Cognome:</label><input type="text" id="cognome" name="cognome"><br>
     <label for="email">Email:</label><input type="email" id="email" name="email"><br>
-    <input type="submit" value="Invia">
+    <input type="submit" name="submit" value="Invia">
   </form>
 FORM;
 
